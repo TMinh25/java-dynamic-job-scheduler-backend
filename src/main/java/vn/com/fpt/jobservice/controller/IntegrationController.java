f841885684244126070be62c5baa50f5b70e6a83
Nguyễn Trường Minh <minhsip2510@gmail.com>
2024-02-07 15:02:27 +0700

    reformat code

--- src/main/java/vn/com/fpt/jobservice/controller/IntegrationController.java
+++ src/main/java/vn/com/fpt/jobservice/controller/IntegrationController.java
@@ -4,38 +4,19 @@ import com.fpt.fis.integration.grpc.ExecuteIntegrationResult;
 import com.fpt.fis.integration.grpc.GetIntegrationListResult;
 import com.fpt.fis.integration.grpc.GetIntegrationResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import vn.com.fpt.jobservice.entity.StepHistory;
-import vn.com.fpt.jobservice.model.StepHistoryModel;
 import vn.com.fpt.jobservice.model.response.IntegrationStructure;
-import vn.com.fpt.jobservice.repositories.StepHistoryRepository;
 import vn.com.fpt.jobservice.service.impl.IntegrationServiceGrpc;
 import vn.com.fpt.jobservice.utils.Utils;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/integrations")
 public class IntegrationController {
     @Autowired
     IntegrationServiceGrpc integrationServiceGrpc;
-
-    @Autowired
-    StepHistoryRepository stepHistoryRepository;
-
-    @GetMapping("testtest")
-    @Transactional
-    public List<StepHistoryModel> GetTestTest() {
-        List<StepHistory> stepHistories =  stepHistoryRepository.findByTaskHistoryIdOrderByStepDesc(1L);
-        return stepHistories.stream()
-                .map(StepHistory::toModel)
-                .collect(Collectors.toList());
-    }
 
     @GetMapping()
     public GetIntegrationListResult getIntegrationGRPC() {
